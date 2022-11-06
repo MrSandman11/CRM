857957754cc0d0661f451441887f843b4aefc3dd
Artem Nikitin <nikitin1111@inbox.ru>
2022-11-06 18:42:46 +0300

    Lesson_4_1

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const name = 'Навигационная система Soundmax';
+const count = 5;
+const category = 'Home Appliances';
+const price = 100;
+
+console.log('name: ', name);
+
+const sum = price * count;
+console.log('sum: ', sum);
+
+
