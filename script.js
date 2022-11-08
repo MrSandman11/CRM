ea5e87adf93e29c5f203ed77d5c4de542c4dd18a
Artem Nikitin <nikitin1111@inbox.ru>
2022-11-08 14:20:39 +0300

    Lesson_4_2

--- script.js
+++ script.js
@@ -10,4 +10,11 @@ console.log('name: ', name);
 const sum = price * count;
 console.log('sum: ', sum);
 
+const name1 = prompt('Наименование товара?');
+const count1 = +prompt('Количество товара?');
+const category1 = prompt('Категория товара?');
+const price1 = +prompt('Цена товара?');
 
+const sum1 = price1 * count1;
+
+console.log(`"На складе ${count1} единицы товара "${name1}" на сумму ${sum1} деревянных"`);
