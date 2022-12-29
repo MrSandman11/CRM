f802dec93c8d8fa807e44c79dd7402afcaf6c12a
Artem Nikitin <nikitin1111@inbox.ru>
2022-12-29 19:53:45 +0300

    Homework_ready

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,25 @@
 'use strict';
 
+const activeModal = document.querySelector('.overlay-modal');
+const addGoodBtn = document.querySelector('.table__add-btn');
+const closeModalBtn = document.querySelector('.modal__close');
+
+addGoodBtn.addEventListener('click', () => {
+  activeModal.classList.add('overlay-modal_active');
+});
+
+closeModalBtn.addEventListener('click', () => {
+  activeModal.classList.remove('overlay-modal_active');
+});
+
+const modalTitle = document.querySelector('.modal .form__title');
+// Что за кнопка возле id и сам id??? Не понятно где искать.
+const form = document.querySelector('#modal');
+const formDiscountCheckbox = document.querySelector('.form__checkbox');
+const formDiscountDescr =
+document.querySelector('.form__discount-input-wrapper .form__input');
+const finalPrice = document.querySelector('.form__final-price_count');
+
 const table = document.querySelector('.table__tbody');
 
 const createRow = (obj) => {
